9320b66e7e15f99a90a7474a12863201b7b2fa1c
Mattias almlof <almlof.mattias@gmail.com>
2018-09-19 11:35:57 +0200

    testpic

--- RayCaster_2001/RayCaster_2001.cpp
+++ RayCaster_2001/RayCaster_2001.cpp
@@ -8,23 +8,23 @@
 int main()
 {
 ;
-glm::vec3 picture[255][255];
+glm::vec3 picture;
 
 	std::ofstream img("awesomepic.ppm");
 	img << "P3" << std::endl;
-	img << 255 << " " << 255 << std::endl;
+	img << 800 << " " << 800 << std::endl;
 	img << "255" << std::endl;
 
-	for (int i = 0; i < 255; i++)
+	for (int i = 0; i < 800; i++)
 	{
 
-		for (int j = 0; j < 255; j++)
+		for (int j = 0; j < 800; j++)
 		{
 			//picture[i][j] = fieldImage[i][j].pixelColor * (255.99 / iMax);
-			picture[i][j] = { i % 255, j % 255, i + j % 255 };
+			picture = { i % 255, j % 255, i*j % 255 };
 
-			img << picture[i][j].x << " " << picture[i][j].y << " " << picture[i][j].z << std::endl;
-			std::cout << picture[i][j].x;
+			img << picture.x << " " << picture.y << " " << picture.z << std::endl;
+			std::cout << picture.x;
 		}
 
 	}
