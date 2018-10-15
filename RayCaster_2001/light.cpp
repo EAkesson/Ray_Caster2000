a62a75563fe2264d99274444cdfdda6ca4ede2d4
Mattias almlof <almlof.mattias@gmail.com>
2018-10-15 13:16:59 +0200

    added upwards facing lightsource

--- RayCaster_2001/light.cpp
+++ RayCaster_2001/light.cpp
@@ -9,7 +9,7 @@ Light::Light()
 Light::Light(Vertex & v1, Vertex & v2, Vertex & v3, Vertex & v4, ColorDbl &d, float watt)
 {
 	
-	numOfTriangles = 2;
+	numOfTriangles = 4;
 	triangles = new Triangle[numOfTriangles];
 	//matProp.opacity = 1;//?
 	//matProp.reflectivity = 1;//?
@@ -18,6 +18,8 @@ Light::Light(Vertex & v1, Vertex & v2, Vertex & v3, Vertex & v4, ColorDbl &d, fl
 
 	triangles[0] = Triangle(v1, v4, v2, this);
 	triangles[1] = Triangle(v4, v3, v2, this);
+	triangles[1] = Triangle(v2, v4, v1, this);
+	triangles[1] = Triangle(v2, v3, v4, this);
 	
    /*4---------3
      |         |
